a6c277904992fc546a24b3a68e55c311423dec98
carlgabrielnilsson <carlgabrielnilsson@gmail.com>
2022-11-10 11:12:31 +0100

    Models, Task, Admin, Employee Marked Done. DB CONFIG working. Relational mapping between tables.

---
+++ taskList/src/main/java/com/example/taskList/model/Admin.java
@@ -0,0 +1,60 @@
+package com.example.taskList.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "admin_generator")
+    private Long id;
+
+    @Column(name="username", nullable = false, updatable = false)
+    private String username;
+
+    @Column(name="password", nullable = false, updatable = false)
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
