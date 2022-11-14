8b771cf03f3ffcd51b1a875c1c2d69dbd80b44d2
46730 <laura.dovid@gmail.com>
2022-11-14 13:19:09 +0100

    Create new task button

--- taskList/src/main/java/com/example/taskList/controller/UserController.java
+++ taskList/src/main/java/com/example/taskList/controller/UserController.java
@@ -76,7 +76,7 @@ public class UserController {
         return "redirect:/";
     }
 
-    @PostMapping("/create/user")
+       @PostMapping("/create/user")
     String createUser(Model model, @ModelAttribute User user) {
         boolean UserAddedSuccess;
         if(userRepo.findByUsername(user.getUsername()) == null) {
@@ -84,30 +84,12 @@ public class UserController {
             userRepo.save(user);
             UserAddedSuccess = true;
             model.addAttribute(UserAddedSuccess);
-            return "User created";
-        }
-        if (userRepo.findByUsername(user.getUsername()) != null){
+        } else {
             UserAddedSuccess = false;
             model.addAttribute(UserAddedSuccess);
-            return "User already exist ";
         }
         return "redirect:/";
     }
-
-//    @PostMapping("/create/user")
-//    String createUser(Model model, @ModelAttribute User user) {
-//        boolean UserAddedSuccess;
-//        if(userRepo.findByUsername(user.getUsername()) == null) {
-//            user.setCreatedAt(LocalDateTime.now());
-//            userRepo.save(user);
-//            UserAddedSuccess = true;
-//            model.addAttribute(UserAddedSuccess);
-//        } else {
-//            UserAddedSuccess = false;
-//            model.addAttribute(UserAddedSuccess);
-//        }
-//        return "redirect:/";
-//    }
 
 
 
